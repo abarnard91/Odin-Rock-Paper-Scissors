4638eb4a0f674e1a979d7e220c22de022ccb45a5
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-05-08 14:39:13 -0500

    Create rock-paper-scissors.js

---
+++ rock-paper-scissors.js
@@ -0,0 +1,47 @@
+console.log("welcome to Rock Paper Scissors");
+//assign variables
+let rock="rock";
+let paper="paper";
+let scissors="scissors";
+//scores set to 0
+let computerScore=0;
+let playerScore=0;
+while computerScore && playerScore <=5:
+	
+//prompt player to pick either rock, paper or scissors
+	let playerChoice= prompt("Choose: rock, paper, scissors");
+  function playerChoice(playerChoice){
+  	return playerChoice.toLowerCase();
+    com.log(playerChoice)
+  }
+  
+  
+  
+  
+//computer player randomly chooses rock paper or scissors
+//compare player and computer results:
+//if player picks rock and:
+	//computer picks rock
+    	//result= tie no points
+    //if computer picks paper
+    	//result= computer wins computer gains 1 point
+    //if computer picks scissors
+    	//result= player wins, player gains 1 point
+//if player picks paper and:
+	//computer picks rock
+    	//result= player wins, player gains 1 point 
+    //if computer picks paper
+    	//result= tie no points 
+    //if computer picks scissors
+    	//result= computer wins computer gains 1 point
+//if player picks scissors and:
+	//computer picks rock
+    	//result= computer wins computer gains 1 point
+    //if computer picks paper
+    	//result= player wins, player gains 1 point 
+    //if computer picks scissors
+    	//result= tie no points
+//if a point is scored or a tie, the game is looped to prompt the player to pick
+//if the score for the playor or computer is == 5 the loop breaks
+// if the player has 5 points, display "you win"
+// if the computer has 5 points, display "you lose
