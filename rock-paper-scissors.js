a94be11fa2d3dd389c94d5aa80b10f21328799b7
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-07-05 14:55:17 -0500

    Update rock-paper-scissors.js
    updated JS to add UI changes

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,81 +3,95 @@ var playerScore=0;
 var computerScore=0;
 
 document.getElementById("button").addEventListener("click", (e)=> {
-	document.getElementById("button").innerHTML="Click again to keep playing!"
+	document.getElementById("button").style.visibility="hidden";
+  document.getElementsByClassName("buttons")[0].style.visibility="visible";})
 	//if the score for the playor or computer is == 5 the loop breaks
-	while ((playerScore<5) &&(computerScore<5)){
-		var rock="rock";
-		var paper= "paper";
-		var scissors="scissors";
-		var playerChoice=prompt("Choose rock, paper, or scissors");
-
+	//while ((playerScore<5) &&(computerScore<5)){
+var rock="rock";
+var paper= "paper";
+var scissors="scissors";
+var playerChoice=""
+document.getElementById("rock").addEventListener("click", ()=> {
+  playerMove("rock");
+  computerMove();
+  compareResults();
+  });
+document.getElementById("paper").onclick=()=>{
+	playerMove("paper");
+	computerMove();
+	compareResults();
+  }
+document.getElementById("scissors").onclick=()=>{
+	playerMove("scissors");
+	computerMove();
+	compareResults();
+  }
+    
 		
 		
 
-		function playerMove(x) {
-				x = x.toLowerCase();
-		playerChoice=x
-				console.log(`player entered ${x}`);
-				if (x !== "rock" && x !== "paper" && x !== "scissors") {
-					x = prompt("incorrect choice. Please choose: rock, paper, scissors");
-			playerChoice=x.toLowerCase();
-					console.log(`if loop response ${x}`);
-				}
-				console.log(`player choice is ${playerChoice}`);
+function playerMove(x) {
+			console.log(`player entered ${x}`);
+       playerChoice=x;
+       return playerChoice;
 		
 		}
 
 		//computer player randomly chooses rock paper or scissors
-		function computerMove() {
-				let randomMove = Math.floor(Math.random() * 2);
-				if (randomMove === 0) {
-					computerChoice = "rock";
-				}
-				else if (randomMove === 1) {
-					computerChoice = "paper";
-				}
-				else if (randomMove === 2) {
-					computerChoice = "scissors";
-				}
-				console.log(`computer choice is ${computerChoice}`);
-		}
-		playerMove(playerChoice);
-		computerMove();
-
-		console.log(`player choice outside of function is ${playerChoice} and computer choice is ${computerChoice}`)
+function computerMove() {
+  let randomMove = Math.floor(Math.random() * 2);
+  if (randomMove === 0) {
+    computerChoice = "rock";
+  }
+  else if (randomMove === 1) {
+    computerChoice = "paper";
+  }
+  else if (randomMove === 2) {
+    computerChoice = "scissors";
+  }
+  console.log(`computer choice is ${computerChoice}`);
+}
 
 		//compare player and computer results:
-		if (playerChoice ===computerChoice){
-			console.log("it's a tie. NO POINTS!!! push button again!");
-			break;
-		}
-		if (((playerChoice === paper) && (computerChoice===scissors))|| ((playerChoice=== rock) &&(computerChoice===paper)) ||((playerChoice===scissors)&&(computerChoice===rock))){
-			console.log("you Lose! Good Day Sir!");
-			computerScore+=1
-			console.log(`The score is player ${playerScore} computer ${computerScore}`);
-			break;
-		}
-		if  (((playerChoice === scissors) && (computerChoice===paper))|| ((playerChoice=== paper) &&(computerChoice===rock)) ||((playerChoice===rock)&&(computerChoice===scissors))){
-			console.log("You've won the round!!!! huzzah! 1 point to you")
-			playerScore+=1
-			console.log(`The score is player ${playerScore} computer ${computerScore}`)
-			break
-		}
-	}
-				
-	// if the player has 5 points, display "you win"
-	if (playerScore==5){
-		document.getElementById("header").innerHTML="YOU WON!!!!";
-		document.getElementById("instructions").innerHTML="refresh the page to play again";
-		document.getElementById("button").innerHTML="Click me all you want. I don't do anything."
-	}
+function compareResults(){
+  if (playerChoice ===computerChoice){
+    console.log("it's a tie. NO POINTS!!! push button again!");
 
-	// if the computer has 5 points, display "you lose
-	if (computerScore==5){
-		document.getElementById("header").innerHTML="YOU LOSE GO HOME!";
-		document.getElementById("instructions").innerHTML="refresh the page to play again";
-		document.getElementById("button").innerHTML="Click me all you want. I don't do anything."
-	}
+      }
+  if (((playerChoice === paper) && (computerChoice===scissors))|| ((playerChoice=== rock) &&(computerChoice===paper)) ||((playerChoice===scissors)&&(computerChoice===rock))){
+    console.log("you Lose! Good Day Sir!");
+    computerScore+=1
+    console.log(`The score is player ${playerScore} computer ${computerScore}`);
 
-})
+      }
+  if  (((playerChoice === scissors) && (computerChoice===paper))|| ((playerChoice=== paper) &&(computerChoice===rock)) ||((playerChoice===rock)&&(computerChoice===scissors))){
+    console.log("You've won the round!!!! huzzah! 1 point to you")
+    playerScore+=1
+    console.log(`The score is player ${playerScore} computer ${computerScore}`)
+    }
 
+
+    // if the player has 5 points, display "you win"
+  if (playerScore===5){
+    document.getElementById("header").innerHTML="YOU WON!!!!";
+    document.getElementById("instructions").innerHTML="refresh the page to play again";
+    document.getElementsByClassName("buttons")[0].style="hidden";
+    document.getElementById("button").style.visibility="visible";
+    document.getElementById("button").innerHTML="Click me to play again."
+    playerScore=0;
+    computerScore=0;
+    return playerScore, computerScore;
+  }
+
+  // if the computer has 5 points, display "you lose"
+  if (computerScore===5){
+    document.getElementById("header").innerHTML="YOU LOSE GO HOME!";
+    document.getElementById("instructions").innerHTML="refresh the page to play again";
+    document.getElementsById("button").innerHTML="Click me to play again."
+    document.getElementsByClassName("buttons")[0].style="hidden";
+    document.getElementById("button").style.visibility="visible";
+    playerScore=0;
+    computerScore=0;
+    return playerScore, computerScore;    
+  }
+}
