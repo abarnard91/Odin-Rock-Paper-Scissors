b6d101dc0cf26ee272b8e96bb9e02fc22f469dc7
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-07-06 13:33:04 -0500

    Update rock-paper-scissors.js
    Fixed addition of div in compareResults() function
    updated messages for winning and losing instructions div

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -57,28 +57,36 @@ function compareResults(){
   const score=document.getElementById("instructions");
   const preScore=document.createElement("div");
   preScore.setAttribute('id','prescore');
+  preScore.setAttribute('class','instructions')
   score.appendChild(preScore);
+  
   if (playerChoice ===computerChoice){
     preScore.textContent = "it's a tie. NO POINTS!!! push button again!";
-
+    
+    
       }
   if (((playerChoice === paper) && (computerChoice===scissors))|| ((playerChoice=== rock) &&(computerChoice===paper)) ||((playerChoice===scissors)&&(computerChoice===rock))){
-    preScore.textContent = "you Lose! Good Day Sir!";
+  	
+   	preScore.textContent = "you Lose! Good Day Sir!";
     computerScore+=1
     score.textContent = `The score is player ${playerScore} computer ${computerScore}`;
-
-      }
+    score.appendChild(preScore);
+   
+		}
   if  (((playerChoice === scissors) && (computerChoice===paper))|| ((playerChoice=== paper) &&(computerChoice===rock)) ||((playerChoice===rock)&&(computerChoice===scissors))){
+  	
     preScore.textContent = "You've won the round!!!! huzzah! 1 point to you";
     playerScore+=1
     score.textContent=`The score is player ${playerScore} computer ${computerScore}`;
+    score.appendChild(preScore);
+    
     }
 
 
     // if the player has 5 points, display "you win"
   if (playerScore===5){
     document.getElementById("header").innerHTML="YOU WON!!!!";
-    document.getElementById("instructions").innerHTML="refresh the page to play again";
+    document.getElementById("instructions").innerHTML="Click the button or Close the page there's nothing else to do here ";
     document.getElementsByClassName("buttons")[0].style="hidden";
     document.getElementById("button").style.visibility="visible";
     document.getElementById("button").innerHTML="Click me to play again."
@@ -90,7 +98,7 @@ function compareResults(){
   // if the computer has 5 points, display "you lose"
   if (computerScore===5){
     document.getElementById("header").innerHTML="YOU LOSE GO HOME!";
-    document.getElementById("instructions").innerHTML="refresh the page to play again";
+    document.getElementById("instructions").innerHTML="Click the button or Close the page there's nothing else to do here ";
     document.getElementsById("button").innerHTML="Click me to play again."
     document.getElementsByClassName("buttons")[0].style="hidden";
     document.getElementById("button").style.visibility="visible";
